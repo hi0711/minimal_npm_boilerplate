498869ede6fd4dd4a7cc6ac951f13b44073cde5a
hi0711 <hiroshi_sato@life-technology.jp>
2017-12-08 19:33:14 +0900

    Updated files

---
+++ mkdir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p assets/scss assets/img assets/img assets/tmpl assets/js
+mkdir -p public/assets/css public/assets/js public/assets/img
+touch assets/scss/common.scss assets/js/common.js
